64048c625d3b1ea8c54868dd19d3f4c9a8dee8b8
Chai Nunes <chaiteanunes@gmail.com>
2016-11-17 08:38:44 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,22 @@
 var squareCount;
 var squareSize;
 
-class Square {
-    constructor(x, y, color) {
-        this.x = x;
-        this.y = y;
-        this.color = color;
-    }
-    
-    draw() {
+var Square = function (x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+}
+
+Square.prototype = {
+    draw: function () {
         push();
         fill(color);
         rect(this.x, this.y, size, size);
         pop();
-    }
+    }   
 }
 
-function setup() {
+function setup () {
     createCanvas(1080, 720);
     noStroke();
     background(255);
@@ -24,6 +24,7 @@ function setup() {
     squareSize = width / squareCount;
 }
 
-function draw() {
+function draw () {
     var square = new Square(0, 0, 0);
+    square.draw();
 }
