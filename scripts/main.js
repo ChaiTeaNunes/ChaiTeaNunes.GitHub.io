8ee93d9bd41138b3d96addebc6d853fea05f4be2
Chai Nunes <chaiteanunes@gmail.com>
2016-11-22 08:42:18 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,6 @@ function instantiateSquares() {
         squares.push([]);
         for (var j = 0; j < squareCount + 2; j++) {
             square = new Square((i - 2) * squareSize, (j - 2) * squareSize, ((i + (j % 2)) % 2) * 255);
-            console.log(square.x + ", " + square.y);
             squares[i].push(square);
         }
     }
@@ -30,6 +29,7 @@ function drawSquares() {
         for (var j = 0; j < squares[i].length; j++) {
             if (frameCount % (squareSize * 2) < squareSize) {
                 squares[i][j].x += (j % 2) * 2;
+                console.log(x);
             }
             squares[i][j].draw();
             if (squares[i][j].x >= width + squareSize) {
