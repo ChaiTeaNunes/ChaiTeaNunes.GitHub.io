85535688acb50e20336923efbc7ef82dead174de
Chai Nunes <chaiteanunes@gmail.com>
2016-11-22 08:12:09 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -17,7 +17,7 @@ function instantiateSquares() {
     for (var i = 0; i < squareCount + 2; i++) {
         squares.push([]);
         for (var j = 0; j < squareCount + 2; j++) {
-            square = new Square((i - 2) * squareSize + (j % 2) * 2, (j - 2) * squareSize, ((i + (j % 2)) % 2) * 255);
+            square = new Square((i - 2) * squareSize, (j - 2) * squareSize, ((i + (j % 2)) % 2) * 255);
             console.log(square.x + ", " + square.y);
             squares[i].push(square);
         }
@@ -28,16 +28,18 @@ function drawSquares() {
     for (var i = 0; i < squares.length; i++) {
         for (var j = 0; j < squares[i].length; j++) {
             if (frameCount % (squareSize * 2) < squareSize) {
-                squares[i][j].x += (j % 2) * 2;
+                if ((squares[i][j].x + squareSize) % squareSize != 0) {
+                    squares[i][j].x += (j % 2) * 2;
+                }
             } else {
-                squares[i][j].y += (i % 2) * 2;   
+                squares[i][j].y += (i % 2) * 2;
             }
             squares[i][j].draw();
             if (squares[i][j].x >= width + squareSize) {
                 squares[i][j].x = -squareSize;
             }
             if (squares[i][j].y >= width + squareSize) {
-                squares[i][j].y = -squareSize;   
+                squares[i][j].y = -squareSize;
             }
         }
     }
