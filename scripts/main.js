f29e48ab0f7bc22d1ebd3f23ab61601c62b3d429
Chai Nunes <chaiteanunes@gmail.com>
2016-11-22 08:38:12 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -30,6 +30,8 @@ function drawSquares() {
         for (var j = 0; j < squares[i].length; j++) {
             if (frameCount % (squareSize * 2) <= squareSize) {
                 squares[i][j].x += (j % 2) * 2;
+            } else if ((squares[i][j].x + squareSize) % (squareSize * 2) != 0) {
+                squares[i][j].x++;
             }
             squares[i][j].draw();
             if (squares[i][j].x >= width + squareSize) {
